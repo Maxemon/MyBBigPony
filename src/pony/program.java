1e71a197f77cde340e5fd0858065efdcc9daf135
s0s100 <s0s100@mail.ru>
2015-03-10 20:48:13 +0400

    Random Games
    Special for u Max!

--- src/pony/program.java
+++ src/pony/program.java
@@ -1,12 +1,36 @@
 package pony;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * Created by Кукуша on 10.03.2015.
  */
 public class program {
     public static void main(String[] args) {
-        System.out.print("Hello World!");
-        System.out.print("Kitty");
+        int a = (int) (100 * Math.random());
+        int vv;
+        System.out.print(a);
+        boolean b= true;
+        Scanner sc = new Scanner(System.in);
+        while(b) {
+            System.out.println(" Введите число");
+            vv = sc.nextInt();
+            if  (vv<a){
+                System.out.println(" Данное число меньше а");
+            } else if (vv>a) {
+                System.out.println(" Данное число больше а");
+            } else {
+                System.out.print("Молодец");
+                b= false;
+            }
+        }
+
+ /*       for(int i = 0;  i<100; ++i){
+            System.out.println(i);
+        }
+
+*/
 
     }
 }
