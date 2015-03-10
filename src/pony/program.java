0474dc8905513a116fdd3021caba7749ac059e4f
s0s100 <s0s100@mail.ru>
2015-03-10 19:39:26 +0400

    first commit

---
+++ src/pony/program.java
@@ -0,0 +1,11 @@
+package pony;
+
+/**
+ * Created by Кукуша on 10.03.2015.
+ */
+public class program {
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+
+    }
+}
